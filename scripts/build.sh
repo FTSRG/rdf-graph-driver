e8ebec75f4287e60dd13f2fe3ca66e8474761aef
Gabor Szarnyas <szarnyasg@gmail.com>
2014-09-23 11:45:15 +0200

    Added build script and configuration for Travis CI

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#*******************************************************************************
+# Copyright (c) 2010-2014, Gabor Szarnyas, Istvan Rath and Daniel Varro
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# Gabor Szarnyas - initial API and implementation
+#*******************************************************************************
+#!/bin/bash
+
+cd "$( cd "$( dirname "$0" )" && pwd )/.."
+
+export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
+mvn clean install || exit 1

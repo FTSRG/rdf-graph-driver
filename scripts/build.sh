a83855aacdc0f6b30bc4bf75a3ac6acc4ce5d48a
Gabor Szarnyas <szarnyasg@gmail.com>
2014-11-14 19:03:34 +0100

    Removed the UnixUtils class as it's now in the mondo-utils project. Also added a scripts for resolving this dependency.

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,5 +12,26 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
+while [ "$1" != "" ]; do
+	case $1 in
+		"--skipTests")
+		skipTests=true
+		;;
+		"--resolveDependencies")
+		resolveDependencies=true
+		;;
+	esac
+	shift
+done
+
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
-mvn clean install || exit 1
+
+if [ $resolveDependencies ]; then
+	scripts/resolve-dependencies.sh || exit 1
+fi
+
+if [ $skipTests ]; then
+	mvn clean install -DskipTests || exit 1
+else
+	mvn clean install || exit 1
+fi

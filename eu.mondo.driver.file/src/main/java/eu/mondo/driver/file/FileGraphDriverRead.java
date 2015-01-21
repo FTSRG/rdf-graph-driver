a5bb486761b921911dd98fa5af484614ced0db66
Dénes Harmath <harmathdenes@gmail.com>
2015-01-21 12:14:22 +0100

    Fix proper method

--- eu.mondo.driver.file/src/main/java/eu/mondo/driver/file/FileGraphDriverRead.java
+++ eu.mondo.driver.file/src/main/java/eu/mondo/driver/file/FileGraphDriverRead.java
@@ -30,113 +30,114 @@ import eu.mondo.driver.graph.util.LiteralParser;
 
 public class FileGraphDriverRead implements RDFGraphDriverRead {
 
-	protected static final String RDF_BASE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-	protected static final String RDF_TYPE = RDF_BASE + "type";
+    protected static final String RDF_BASE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    protected static final String RDF_TYPE = RDF_BASE + "type";
 
-	protected final Collection<Statement> statements = new HashSet<>();
+    protected final Collection<Statement> statements = new HashSet<>();
 
-	protected final String regex = ".*x(\\d+)";
-	protected final Pattern pattern = Pattern.compile(regex);
+    protected final String regex = ".*x(\\d+)";
+    protected final Pattern pattern = Pattern.compile(regex);
 
-	public FileGraphDriverRead(final String connectionString) throws RDFParseException, RDFHandlerException, IOException {
-		RDFFormat format = Rio.getParserFormatForFileName(connectionString);
-		RDFParser parser = Rio.createParser(format);
-		StatementCollector collector = new StatementCollector(statements);
-		parser.setRDFHandler(collector);
+    public FileGraphDriverRead(final String connectionString) throws RDFParseException, RDFHandlerException, IOException {
+        RDFFormat format = Rio.getParserFormatForFileName(connectionString);
+        RDFParser parser = Rio.createParser(format);
+        StatementCollector collector = new StatementCollector(statements);
+        parser.setRDFHandler(collector);
 
-		String filePath = connectionString.split("rdf://")[1];
-		InputStream in = new FileInputStream(filePath);
-		String baseURI = "";
-		parser.parse(in, baseURI);
-	}
+        String filePath = connectionString.split("rdf://")[1];
+        InputStream in = new FileInputStream(filePath);
+        String baseURI = "";
+        parser.parse(in, baseURI);
+    }
 
-	@Override
-	public long countVertices(final String type) {
-		return collectVertices(type).size();
-	}
+    @Override
+    public long countVertices(final String type) {
+        return collectVertices(type).size();
+    }
 
-	@Override
-	public long countEdges(String type) {
-		return collectEdges(type).size();
-	}
+    @Override
+    public long countEdges(String type) {
+        return collectEdges(type).size();
+    }
 
-	@Override
-	public long countProperties(String type) {
-		return collectProperties(type).size();
-	}
+    @Override
+    public long countProperties(String type) {
+        return collectProperties(type).size();
+    }
 
-	@Override
-	public List<Long> collectVertices(final String type) {
-		List<Long> vertices = new ArrayList<>();
+    @Override
+    public List<Long> collectVertices(final String type) {
+        List<Long> vertices = new ArrayList<>();
 
-		URI rdfType = new URIImpl(RDF_TYPE);
-		URI vertexType = new URIImpl(type);
+        URI rdfType = new URIImpl(RDF_TYPE);
+        URI vertexType = new URIImpl(type);
 
-		for (Statement statement : statements) {
-			if (statement.getPredicate().equals(rdfType) && statement.getObject().equals(vertexType)) {
-				Long id = extractId(statement.getSubject().toString());
-				vertices.add(id);
-			}
-		}
-		return vertices;
-	}
+        for (Statement statement : statements) {
+            if (statement.getPredicate().equals(rdfType) && statement.getObject().equals(vertexType)) {
+                Long id = extractId(statement.getSubject().toString());
+                vertices.add(id);
+            }
+        }
+        return vertices;
+    }
 
-	@Override
-	public Multimap<Long, Long> collectEdges(final String type) {
-		Multimap<Long, Long> edges = ArrayListMultimap.create();
-		URI edgeType = new URIImpl(type);
+    @Override
+    public Multimap<Long, Long> collectEdges(final String type) {
+        Multimap<Long, Long> edges = ArrayListMultimap.create();
+        URI edgeType = new URIImpl(type);
 
-		for (Statement statement : statements) {
-			if (statement.getPredicate().equals(edgeType)) {
-				Long v0 = extractId(statement.getSubject().toString());
-				Long v1 = extractId(statement.getObject().toString());
-				edges.put(v0, v1);
-			}
-		}
-		return edges;
-	}
+        for (Statement statement : statements) {
+            if (statement.getPredicate().equals(edgeType)) {
+                try {
+                    Long subjectId = extractId(statement.getSubject().toString());
+                    Long objectId = extractId(statement.getObject().toString());
+                    edges.put(subjectId, objectId);
+                } catch (IllegalStateException e) {
+                    // drop statement if id is not valid
+                }
+            }
+        }
+        return edges;
+    }
 
-	@Override
-	public Multimap<Long, Object> collectProperties(final String type) {
-		Multimap<Long, Object> properties = ArrayListMultimap.create();
-		URI propertyType = new URIImpl(type);
+    @Override
+    public Multimap<Long, Object> collectProperties(final String type) {
+        Multimap<Long, Object> properties = ArrayListMultimap.create();
+        URI propertyType = new URIImpl(type);
 
-		for (Statement statement : statements) {
-			if (statement.getPredicate().equals(propertyType)) {
-				try {
-					Long id = extractId(statement.getSubject().toString());
-					Value value = statement.getObject();
-					if (value instanceof Literal) {
-						Literal literal = (Literal) value;
-						Object object = LiteralParser.literalToObject(literal);
-						properties.put(id, object);
-					}				
-				} catch (IllegalStateException e) {
-					// drop statement
-				}
-			}
-		}
+        for (Statement statement : statements) {
+            if (statement.getPredicate().equals(propertyType)) {
 
-		return properties;
-	}
+                Long id = extractId(statement.getSubject().toString());
+                Value value = statement.getObject();
+                if (value instanceof Literal) {
+                    Literal literal = (Literal) value;
+                    Object object = LiteralParser.literalToObject(literal);
+                    properties.put(id, object);
+                }
+            }
+        }
 
-	protected Long extractId(final String string) {
-		final Matcher matcher = pattern.matcher(string);
-		if (matcher.matches()) {
-			return new Long(matcher.group(1));
-		} else {
-			throw new IllegalStateException("No match found.");
-		}
-	}
+        return properties;
+    }
 
-	@Override
-	public List<String> getVertexTypes() throws IOException {
-		throw new UnsupportedOperationException();
-	}
+    protected Long extractId(final String string) {
+        final Matcher matcher = pattern.matcher(string);
+        if (matcher.matches()) {
+            return new Long(matcher.group(1));
+        } else {
+            throw new IllegalStateException("No match found.");
+        }
+    }
 
-	@Override
-	public List<String> getEdgeTypes() throws IOException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public List<String> getVertexTypes() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> getEdgeTypes() throws IOException {
+        throw new UnsupportedOperationException();
+    }
 
 }
